86dffd9226c49895458008e88ca060713fd3af05
Sukhdeep Singh Saini <sukhdeepsaini@yahoo.com>
2014-11-24 23:14:27 -0500

    Jquery in Angular
    Testing some Jquery in Angular if its works fine. Having some issues,
    Working on it.

---
+++ js/app.js
@@ -0,0 +1,60 @@
+
+var myWeb = angular.module('myWebApp',['ngRoute']);
+
+$(document).ready(function() {
+    var colorOrig=$(".hovereffect").css('background-color');
+    $(".hovereffect").hover(
+    function() {
+        //mouse over
+        $(this).css('background', '#ff0')
+    }, function() {
+        //mouse out
+        $(this).css('background', colorOrig)
+    });
+});
+
+myWeb.config(['$routeProvider',function ($routeProvider){
+
+	$routeProvider
+		.when('/',
+		{
+			controller: 'NavbarController',
+			templateUrl:'home.html'
+
+		})
+		.when('/profile',
+		{
+			controller: 'NavbarController',
+			templateUrl:'profile.html'
+
+		})
+		.when('/projects',
+		{
+			controller: 'ProjectsController',
+			templateUrl:'projects.html'
+
+		})
+		.when('/contact',
+		{
+			controller: 'NavbarController',
+			templateUrl:'contact.html'
+
+		})
+		.when('/recipes/:recipename',
+		{
+			controller: 'RecipeDetailCtrl',
+			templateUrl:'recipedetails.html'
+
+		})
+        .when('/recipes',
+        {
+            controller: 'ProjectsController',
+            templateUrl:'recipes.html'
+
+        })
+		.otherwise({redirectTo:'/'});
+
+
+}]);
+
+
